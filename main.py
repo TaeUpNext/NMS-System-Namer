cc50b7360d3a6b1e8d6434afe3d896c953ec40a2
Taeupnext <armontaeh17@gmail.com>
2022-03-12 12:07:11 -0500

    added functionality to name systems using 2 words

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ gib = Gibberish()
 
 program = True
 while program:
-    numWords = randint(1,1)
+    numWords = randint(2,2)
     if numWords == 1: #If only 1 word will be generated
         gibVowel = randint(1,3)
         if gibVowel == 1:
@@ -18,7 +18,56 @@ while program:
 
         elif gibVowel == 3: #prints 1 word that ends with a vowel
             print(f"\n{gib.generate_word(end_vowel= True)}")
+    
+    #creates 2 words       
+    elif numWords == 2:  #creates 2 words
+        gibVowel1 = randint(2,2)
+        gibVowel2 = randint(2,2)
+        if gibVowel1 == 1 and gibVowel2 == 1: #both words do not start or end in a vowel
+            words = gib.generate_words(2)
+            print("\n", *words)
         
+        elif gibVowel1 == 1 and gibVowel2 == 2:# 1st word doesnt star/end in vowel, 2nd word starts with vowel
+            word1 = gib.generate_word()
+            word2 = gib.generate_word(start_vowel = True)
+            print(f"\n {word1} {word2}")
+        
+        elif gibVowel1 == 1 and gibVowel2 == 3: #1st word doesnt star/end in vowel, 2nd word ends with vowel
+            word1 = gib.generate_word()
+            word2 = gib.generate_word(end_vowel = True)
+            print(f"\n {word1} {word2}")
+        
+        elif gibVowel1 == 2 and gibVowel2 == 1: #1st word starts with a vowel, 2nd  word doesn't star or end in vowel
+            word1 = gib.generate_word(start_vowel = True)
+            word2 = gib.generate_word()
+            print(f"\n {word1} {word2}")
+        
+        elif gibVowel1 == 2 and gibVowel2 == 2: #both words start with a vowel
+            word1 = gib.generate_word(start_vowel = True)
+            word2= gib.generate_word(start_vowel = True)
+            print(f"\n {word1} {word2}")
+        
+        elif gibVowel1 == 2 and gibVowel2 == 3: #1st word starts a vowel and the 2nd word ends in a vowel
+            word1 = gib.generate_word(start_vowel = True)
+            word2 = gib.generate_word(end_vowel = True)
+            print(f"\n {word1} {word2}")
+        
+        elif gibVowel1 == 3 and gibVowel2 == 1: #1st word ends in a vowel 2nd word doesnt start or end in a vowel
+            word1 = gib.generate_word(end_vowel = True)
+            word2 = gib.generate_word()
+            
+            print(f"\n {word1} {word2}")
+        
+        elif gibVowel1 == 3 and gibVowel2 == 2: #1st word ends in a vowel 2nd word starts in a vowel
+            word1 = gib.generate_word(end_vowel = True)
+            word2 = gib.generate_word(start_vowel = True)
+            print("\n {word1} {word2}")
+        
+        elif gibVowel1 == 3 and gibVowel2 == 3: #both words end in a vowel
+            word1 = gib.generate_word(end_vowel = True)
+            word2 = gib.generate_word(end_vowel = True)
+            print(f"\n {word1} {word2}")
+            
             
         continueChoice = int(input("\nWould you like to generate another name for a system? Enter a 1 for yes or a 2 for no: ")) #checks to see if the user wants to generate another system name
         if continueChoice == 1:
